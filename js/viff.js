7b99074a37daa64af0577a052ccc993f0d4130d7
Si Xiaojing <sixiaojing1988@gmail.com>
2013-07-29 20:46:56 +0800

    add screenshot folder.and modified json file.

--- js/viff.js
+++ js/viff.js
@@ -1,43 +1,78 @@
 function ViffCtrl ($scope, $http) {
-	$scope.pages = [
-		{browser: "chrome", url: "index.html", production: "production.png", local: "local.png", hasDiff: true},
-		{browser: "chrome", url: "home.html", production: "production.png", local: "local.png", hasDiff: false},
-		{browser: "chrome", url: "detail.html", production: "production.png", local: "local.png", hasDiff: true},
-		{browser: "chrome", url: "contact.html", production: "production.png", local: "local.png", hasDiff: true},
-		{browser: "firefox", url: "home.html", production: "production.png", local: "local.png", hasDiff: true},
-		{browser: "firefox", url: "detail.html", production: "production.png", local: "local.png", hasDiff: false},
-		{browser: "firefox", url: "contact.html", production: "production.png", local: "local.png", hasDiff: true},
-		{browser: "IE", url: "detail.html", production: "production.png", local: "local.png", hasDiff: true},
-		{browser: "IE", url: "index.html", production: "production.png", local: "local.png", hasDiff: true}]
+  // $scope.pages = [
+  //  {browser: "chrome", url: "index.html", production: "production.png", local: "local.png", hasDiff: true},
+  //  {browser: "chrome", url: "home.html", production: "production.png", local: "local.png", hasDiff: false},
+  //  {browser: "chrome", url: "detail.html", production: "production.png", local: "local.png", hasDiff: true},
+  //  {browser: "chrome", url: "contact.html", production: "production.png", local: "local.png", hasDiff: true},
+  //  {browser: "firefox", url: "home.html", production: "production.png", local: "local.png", hasDiff: true},
+  //  {browser: "firefox", url: "detail.html", production: "production.png", local: "local.png", hasDiff: false},
+  //  {browser: "firefox", url: "contact.html", production: "production.png", local: "local.png", hasDiff: true},
+  //  {browser: "IE", url: "detail.html", production: "production.png", local: "local.png", hasDiff: true},
+  //  {browser: "IE", url: "index.html", production: "production.png", local: "local.png", hasDiff: true}]
 
-	$scope.viffPages = function(){
-		var pages = []
-		angular.forEach($scope.pages, function(page){
-			if(page.hasDiff) pages.push(page);
-		});
-		return pages;
-	}
-	 
-	$scope.acceptDiff = function(currentPath){
-		console.log()
-		angular.forEach($scope.pages, function(page){
-			if(page.url == $scope.currentPath && page.browser == $scope.defaultBrowser ) page.hasDiff = false;
-		});
-	}
+  
+  $http.get("report.json").success(function(data){
+    $scope.caseCount = data.caseCount;
+    $scope.pages = []
+    $scope.browsers = []
 
-	$scope.defaultBrowser = "chrome"
-	$scope.currentPath = "index.html"
-	resetImage();
+    angular.forEach(data.compares, function(urls, browser){
+      $scope.browsers.push(browser);
+      angular.forEach(urls, function(properties, url){
+        var viffCase = {};
+        viffCase.browser = browser;
+        viffCase.url = url;
+        viffCase.envs = [];
 
-	$scope.changePath = function(url){
-		$scope.currentPath = url;
-		resetImage();
-	}
+        angular.forEach(properties.images, function(filePath, env) {
+          if(env != 'diff')
+            viffCase.envs.push({ name: env, filePath : filePath });
+        });
 
-	function resetImage(){
-		$scope.local = "reports/"+$scope.defaultBrowser+"/"+ $scope.currentPath.split('.')[0] +"/local.png"
-		$scope.production = "reports/"+$scope.defaultBrowser+"/"+ $scope.currentPath.split('.')[0] +"/production.png"
-		$scope.viff = "reports/"+$scope.defaultBrowser+"/"+ $scope.currentPath.split('.')[0] +"/viff.png"	
-	}
-	// body...
+        viffCase.viff = properties.images['diff'];
+        viffCase.isSameDimensions = properties.isSameDimensions;
+        viffCase.misMatchPercentage = properties.misMatchPercentage;
+        viffCase.analysisTime = properties.analysisTime
+        $scope.pages.push(viffCase);
+      });
+    });
+
+
+
+  $scope.viffPages = function(){
+    var pages = []
+    angular.forEach($scope.pages, function(page){
+      if(!page.isSameDimensions) pages.push(page);
+    });
+    return pages;
+  }
+
+  $scope.acceptDiff = function(){
+    $scope.currentPage.isSameDimensions = true;
+  }
+
+
+  $scope.currentBrowser = $scope.browsers[0];
+
+  $scope.currentPage = $scope.pages[0];
+  resetImage();
+
+  $scope.changePath = function(url){
+    angular.forEach($scope.pages, function(page){
+      if(page.url == url && page.browser == $scope.currentBrowser) $scope.currentPage = page;
+    })
+    resetImage();
+  }
+
+  function resetImage(){
+    $scope.evn1Shot = encodeURIComponent($scope.currentPage.envs[0]["filePath"])
+    $scope.evn2Shot = encodeURIComponent($scope.currentPage.envs[1]["filePath"])
+    $scope.viffShot = encodeURIComponent($scope.currentPage.viff)
+  }
+    
+    
+  });
+
+  
+  // body...
 }
